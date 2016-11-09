b141496a7c2b92b185d25e8539ed8a69a8f549fc
Elwin Mao <elwin_mao@163.com>
2016-11-08 22:32:01 -0500

    mv csv to /result/testcase/

--- joblist/ebizzy.sh
+++ joblist/ebizzy.sh
@@ -1,4 +1,4 @@
 lkp install lkp install $LKP_SRC/jobs/ebizzy.yaml
 lkp split $LKP_SRC/jobs/ebizzy.yaml
 lkp run $LKP_SRC/ebizzy-200%-5x-5s.yaml
-lkp collect -c testcase=ebizzy  -o /result/ebizzy.csv
+lkp collect -c testcase=ebizzy  -o /result/ebizzy/ebizzy.csv
--- joblist/nbench.sh
+++ joblist/nbench.sh
@@ -1,4 +1,4 @@
 lkp install lkp install $LKP_SRC/jobs/nbench.yaml
 lkp split $LKP_SRC/jobs/nbench.yaml
 lkp run $LKP_SRC/nbench-1x.yaml
-lkp collect -c testcase=nbench  -o /result/nbench.csv
+lkp collect -c testcase=nbench  -o /result/nbench/nbench.csv
