5a1dbb65a76a9eb2821ef04af49388e81a08492a
Elwin Mao <elwin_mao@163.com>
2016-11-18 02:10:59 -0500

    copy /data/commitId.txt to /root/commitId.txt

--- testcase/lkpebizzy/fortest.sh
+++ testcase/lkpebizzy/fortest.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -x
-ip_of_android="192.168.2.35"
+ip_of_android="192.168.2.46"
 tmp_result_dir="lkp_tmp_result"
 cd "$(dirname "$0")" 
 apt-get install -y android-tools-adb
 adb connect $ip_of_android
+
+adb -s $ip_of_android:5555   push ./commitId.txt /data/
 
 ./lkpebizzy.sh  $ip_of_android  5555   $tmp_result_dir
--- testcase/lkpjishigou/fortest.sh
+++ testcase/lkpjishigou/fortest.sh
@@ -5,4 +5,6 @@ cd "$(dirname "$0")"
 apt-get install -y android-tools-adb
 adb connect $ip_of_android
 
+adb -s $ip_of_android:5555   push ./commitId.txt /data/
+
 ./lkpjishigou.sh  $ip_of_android  5555   $tmp_result_dir
