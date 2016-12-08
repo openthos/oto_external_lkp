2caf96e99143bab4343b16c55bbff58d17494d50
Elwin Mao <elwin_mao@163.com>
2016-12-07 23:01:15 -0500

    fix csv overwriet bug

--- joblist/jishigou.sh
+++ joblist/jishigou.sh
@@ -1,4 +1,4 @@
 lkp install $LKP_SRC/jobs/jishigou.yaml
 lkp split $LKP_SRC/jobs/jishigou.yaml
 lkp run $LKP_SRC/jishigou-1x.yaml
-lkp collect -c testcase=jishigou -o /result/jishigou/jishigou.csv
+lkp collect -c testcase=jishigou -o /result/jishigou/jishigou_`hostname`.csv
--- joblist/wechat.sh
+++ joblist/wechat.sh
@@ -1,4 +1,4 @@
 lkp install $LKP_SRC/jobs/wechat.yaml
 lkp split $LKP_SRC/jobs/wechat.yaml
 lkp run $LKP_SRC/wechat-1x.yaml
-lkp collect -c testcase=wechat -o /result/wechat/wechat.csv
+lkp collect -c testcase=wechat -o /result/wechat/wechat_`hostname`.csv
