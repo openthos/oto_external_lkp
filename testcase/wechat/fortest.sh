f3d9f429d1ce23a31b0791f5626f5a0b705c97f8
xhl2791188 <847036983@qq.com>
2016-12-20 11:32:09 +0800

    change wechat.Make

--- testcase/wechat/fortest.sh
+++ testcase/wechat/fortest.sh
@@ -4,9 +4,10 @@ tmp_result_dir=$2
 testcasename=$(basename `pwd`)
 cd "$(dirname "$0")" 
 apt-get install -y android-tools-adb
+source /etc/profile
 ./wechatMake.sh
 adb connect $ip_of_android
-adb push ./bin/wechat.jar /data/local/tmp
+adb -s $ip_of_android:5555   push ./bin/wechat.jar /data/local/tmp
 adb -s $ip_of_android:5555   push ./commitId.txt /data/
 
 ./$testcasename.sh  $ip_of_android  5555   $tmp_result_dir
--- testcase/wechat/run_withlog.sh~
+++ testcase/wechat/run_withlog.sh~
@@ -1,5 +1,5 @@
 #!/bin/bash -x
 cd "$(dirname "$0")" 
-ip_of_android="192.168.2.74"
+ip_of_android="192.168.0.139"
 tmp_result_dir="lkp_tmp_result"
 ./fortest.sh $ip_of_android  $tmp_result_dir  2>&1 | tee ./test_log.txt
