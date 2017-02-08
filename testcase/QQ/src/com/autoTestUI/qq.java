39d9aa5fd3d621b6ec2e5f7e0832190c58cadc7e
zhanglinping <18701158269@163.com>
2017-02-08 17:31:43 +0800

    modify qqtest

--- testcase/QQ/src/com/autoTestUI/qq.java
+++ testcase/QQ/src/com/autoTestUI/qq.java
@@ -1,6 +1,9 @@
 package com.autoTestUI;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.os.RemoteException;
 import com.android.uiautomator.core.UiObject;
 import com.android.uiautomator.core.UiObjectNotFoundException;
@@ -20,7 +23,27 @@ public class qq extends UiAutomatorTestCase {
 		otoTest = new otoDisplayRun(getUiDevice());
 		otoTest.mydevice.wakeUp();
 		assertTrue("screen on :can't wakeup", otoTest.mydevice.isScreenOn());
+		//启动时间
+		Date starttime;
+		Date endtime;
+		long launchTime;	
+		SimpleDateFormat format=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS");
 
+		starttime = new Date();
+		System.out.println("----------Start time： " +  format.format(starttime));
+		System.out.println("starttime:" +  System.currentTimeMillis());
+		otoDisplayRun.execCmdNoSave("am start -n " + appName);
+		
+		endtime = new Date();
+		System.out.println("----------结束时间： " +  format.format(endtime));
+		System.out.println("endtime:" +  System.currentTimeMillis());
+		
+		launchTime = endtime.getTime() - starttime.getTime();
+		System.out.println("----------APP launch 时间： " + launchTime +"ms");
+		Thread.sleep(3000);
+		//关闭温馨提示弹窗
+		otoTest.ClickById("android:id/button1");
+		
 		window_lib.windowtest(otoTest.mydevice, appName);
 		otoDisplayRun.execCmdNoSave("am start -n " + appName);
 		sleep(1000);
