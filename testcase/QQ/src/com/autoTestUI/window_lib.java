adb2bed29e3771d3ea2185b0d36afeb5773016ac
zhanglinping <18701158269@163.com>
2017-02-08 16:58:21 +0800

    solve problems of qqtest break

--- testcase/QQ/src/com/autoTestUI/window_lib.java
+++ testcase/QQ/src/com/autoTestUI/window_lib.java
@@ -34,14 +34,14 @@ public class window_lib extends UiAutomatorTestCase{
 		Thread.sleep(3000);
 		
 		// 改变窗体大小 将左边框向右侧拉动 改变大小
-		UiObject objectSide = new UiObject(new UiSelector().resourceId("android:id/mwOuterBorder"));
-		if(IfExist(objectSide,"android:id/mwOuterBorder",device)){
+		if(IfExist("android:id/mwOuterBorder",device)){
+			UiObject objectSide = new UiObject(new UiSelector().resourceId("android:id/mwOuterBorder"));
 			android.graphics.Rect myAppSide = objectSide.getVisibleBounds();
 			device.drag(myAppSide.left, myAppSide.top, myAppSide.left + 100,myAppSide.top, 2);
 			Thread.sleep(2000);
 			// 重新通过resourceId 获取窗口边界坐标，与预计的坐标不相等
-			UiObject objectSide1 = new UiObject(new UiSelector().resourceId("android:id/mwOuterBorder"));
-			if(IfExist(objectSide1,"android:id/mwOuterBorder",device)){
+			if(IfExist("android:id/mwOuterBorder",device)){
+				UiObject objectSide1 = new UiObject(new UiSelector().resourceId("android:id/mwOuterBorder"));
 				android.graphics.Rect myAppSide1 = objectSide1.getVisibleBounds();
 				// 验证上一次拖动是否成功：10 pixcel 的误差
 				if (myAppSide1.left < (myAppSide.left + 40)|| myAppSide1.left > (myAppSide.left + 60)) {
@@ -51,8 +51,8 @@ public class window_lib extends UiAutomatorTestCase{
 				device.drag(myAppSide1.left, myAppSide1.top, myAppSide1.left - 100,myAppSide1.top, 2);
 				Thread.sleep(2000);
 				// 重新通过resourceId 获取窗口边界坐标
-				UiObject objectSide2 = new UiObject(new UiSelector().resourceId("android:id/mwOuterBorder"));
-				if(IfExist(objectSide2,"android:id/mwOuterBorder",device)){
+				if(IfExist("android:id/mwOuterBorder",device)){
+					UiObject objectSide2 = new UiObject(new UiSelector().resourceId("android:id/mwOuterBorder"));
 					android.graphics.Rect myAppSide2 = objectSide2.getVisibleBounds();
 					// 验证上一次拖动是否成功
 					if (myAppSide2.left < (myAppSide.left - 10)|| myAppSide2.left > (myAppSide.left + 10)) {
@@ -62,8 +62,8 @@ public class window_lib extends UiAutomatorTestCase{
 					device.drag(myAppSide2.right - 1, myAppSide2.bottom - 1,myAppSide2.right + 100, myAppSide2.bottom + 100, 2);
 					Thread.sleep(2000);
 					// 重新通过resourceId 获取窗口边界坐标 下面拖动时，差几个像素点到边界 所以-2
-					UiObject objectSide3 = new UiObject(new UiSelector().resourceId("android:id/mwOuterBorder"));
-					if(IfExist(objectSide3,"android:id/mwOuterBorder",device)){
+					if(IfExist("android:id/mwOuterBorder",device)){
+						UiObject objectSide3 = new UiObject(new UiSelector().resourceId("android:id/mwOuterBorder"));
 						android.graphics.Rect myAppSide3 = objectSide3.getVisibleBounds();
 						// 验证上一次拖动是否成功
 						if (myAppSide3.right < (myAppSide2.right + 40)|| myAppSide3.right > (myAppSide2.right + 60)) {
@@ -77,8 +77,8 @@ public class window_lib extends UiAutomatorTestCase{
 		}
 		
 		// 拖动程序 拖动程序后， 窗口最大化/最小化等位置将无法通过resourceId获取到
-		UiObject objectHead = new UiObject(new UiSelector().resourceId("android:id/mw_decor_header"));
-		if(IfExist(objectHead,"android:id/mw_decor_header",device)){
+		if(IfExist("android:id/mw_decor_header",device)){
+			UiObject objectHead = new UiObject(new UiSelector().resourceId("android:id/mw_decor_header"));
 			objectHead.dragTo(1000, 500, 10);
 			Thread.sleep(1000);
 		}
@@ -86,7 +86,8 @@ public class window_lib extends UiAutomatorTestCase{
 		// 强制关闭程序
 		otoDisplayRun.execCmdNoSave("am force-stop " + appName.substring(0, appName.indexOf("/")));
 	}
-	public static boolean IfExist(UiObject myobject,String str,UiDevice mydevice) throws InterruptedException{
+	public static boolean IfExist(String str,UiDevice mydevice) throws InterruptedException{
+		UiObject myobject = new UiObject(new UiSelector().resourceId(str));
 		int i = 0;
 		while(!myobject.exists() && i < 5){
 //			SolveProblems();
