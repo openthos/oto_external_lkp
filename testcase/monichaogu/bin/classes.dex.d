c834d4af4e29037aa7c34aaa9cfb1f84a6fd0885
jiangqin0501 <hecateqin@sina.com>
2017-09-15 15:49:17 +0800

    remove some useless files of monichaogu

--- testcase/monichaogu/bin/classes.dex.d
+++
@@ -1,4 +0,0 @@
-/root/oto_external_lkp/testcase/monichaogu/bin/classes.dex : \
-/root/oto_external_lkp/testcase/monichaogu/bin/classes/com/autoTestUI/monichaoguTest1.class \
-/root/oto_external_lkp/testcase/monichaogu/bin/classes/com/autoTestUI/otoDisplayRun.class \
-/root/oto_external_lkp/testcase/monichaogu/bin/classes/com/autoTestUI/window_lib.class \
