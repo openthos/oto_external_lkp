9b358e1860040786da3abafbd9c45307c1b97ead
zhanglinping <18701158269@163.com>
2017-11-17 13:33:05 +0800

    add common code

--- testcase/YY/lkp_test/before_chroot.sh
+++
@@ -1,3 +0,0 @@
-cd /data/
-/system/xbin/telnetd -l /system/xbin/sh
-cd -
--- testcase/date/dateMake.sh
+++ testcase/date/dateMake.sh
@@ -1,5 +1,1 @@
-#!/bin/bash -x
-#需要根据环境修改-t参数(现在是1)，查看5.1版本对应android stdio的id（ android list target )
-
-android create uitest-project -n date -t 1 -p .
-ant build
+../common/commonMake.sh
